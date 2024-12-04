593e3cf934903456c2d44fe07614c691a782ae87
StevePenguin <dogiahuy2252004@gmail.com>
2024-12-05 00:29:59 +0700

    init project

---
+++ src/main/java/tdtu/Proptech/ProptechApplication.java
@@ -0,0 +1,13 @@
+package tdtu.Proptech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProptechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProptechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tdtu/Proptech/ProptechApplicationTests.java
@@ -0,0 +1,13 @@
+package tdtu.Proptech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProptechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
